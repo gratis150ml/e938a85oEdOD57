a3c9142cdfa0ab1832226a5f6d6c96674cf7145c
gratis150ml <98968183+gratis150ml@users.noreply.github.com>
2022-03-08 13:14:36 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+$("form").submit(function(e){
+    e.preventDefault();
+    activate();
+});
+url = "https://api.agify.io/?name="
+const find = (name) => fetch(url+name)
+async function activate() {
+    try {
+        $('p').html('<div class="spinner-border text-danger" role="status"></div>')
+        const r = await find($('input').val())
+        const d = await r.json()
+        if(d.age) {
+            $('p').html(`Your age is: ${d.age}`)
+        } else {
+            $('p').html(`Something went wrong`)
+        }
+    } catch (err) {
+        console.log(err)
+    }
+}
